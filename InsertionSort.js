5a54381b05180a4c5970bca8d37ca1198c552a24
rebeccamurphy <rebeccalorenmurphy@gmail.com>
2015-01-02 20:57:17 -0500

    did quicksort and insertsort

---
+++ InsertionSort.js
@@ -0,0 +1,45 @@
+var test = [1, 12, 5, 26, 7, 14, 3, 7, 2];
+
+/*
+	Plain Insertion Sort
+	Shifts values instead of swapping
+	
+	Overall Complexity: O(n^2)
+
+	Insertion sort's overall complexity is O(n2) on average, regardless of the method of insertion. 
+	On the almost sorted arrays insertion sort shows better performance, up to O(n) in case of applying insertion sort to a sorted array. 
+	Number of writes is O(n2) on average, but number of comparisons may vary depending on the insertion algorithm. 
+	It is O(n^2) when shifting or swapping methods are used and O(n log n) for binary insertion sort.
+
+	From the point of view of practical application, an average complexity of the insertion sort is not so important. 
+	As it was mentioned above, insertion sort is applied to quite small data sets (from 8 to 12 elements).
+	Therefore, first of all, a "practical performance" should be considered. 
+	In practice insertion sort outperforms most of the quadratic sorting algorithms, like selection sort or bubble sort.
+
+	Insertion sort properties
+
+		adaptive (performance adapts to the initial order of elements);
+		stable (insertion sort retains relative order of the same elements);
+		in-place (requires constant amount of additional space);
+		online (new elements can be added during the sort);
+
+*/
+
+
+var insertionsort = function(list){
+	var i, j, temp;
+
+	for (i = 1; i < list.length; i++) {
+		//first element checking for swap
+        temp = list[i];
+        j = i;
+        while (j > 0 && list[j - 1] > temp) {
+        	//if a value at the left side of the list is greater than the right side, shift value to right
+            list[j] = list[j - 1];
+            j--;
+        }
+        //when the value is on the correct side, insert value where is is sorted to
+        list[j] = temp;
+     }
+     return list;
+}
---
+++ QuickSort.js
@@ -0,0 +1,60 @@
+var test = [1, 12, 5, 26, 7, 14, 3, 7, 2];
+
+/*
+
+	QuickSort 
+	Complexity: O(n log n)
+
+	The divide-and-conquer strategy is used in quicksort. Below the recursion step is described:
+	1)	Choose a pivot value. We take the value of the middle element as pivot value, 
+		but it can be any value, which is in range of sorted values, even if it doesn't present in the array.
+	2)	Partition. Rearrange elements in such a way, that all elements which are lesser than the pivot 
+		go to the left part of the array and all elements greater than the pivot, go to the right part of the array.
+	 	Values equal to the pivot can stay in any part of the array. Notice, that array may be divided in non-equal parts.
+	3)	Sort both parts. Apply quicksort algorithm recursively to the left and the right parts.
+
+	On the average quicksort has O(n log n) complexity, but strong proof of this fact is not trivial and not presented here. 
+	In worst case, quicksort runs O(n2) time, but on the most "practical" data it works just fine and 
+	outperforms other O(n log n) sorting algorithms.
+*/
+
+
+
+var quicksort = function(list, left, right){
+	left = (left===undefined)? 0: left;
+	right= (right===undefined)? list.length-1:right;
+
+	var i = left;
+	var j = right;
+	var pivotVal = list[Math.floor((left+right)/2)];
+
+
+	console.log("Current list: " + list);
+	console.log("Current Partion: " +list.slice(left, right+1));
+	console.log("Current Pivot Value: " + pivotVal);
+
+	while (i<=j){
+		var temp;
+		while (list[i]<pivotVal)
+			i++;
+		while (list[j]>pivotVal)
+			j--;
+		if (i<=j){
+			temp =list[i];
+			list[i] = list[j];
+			list[j] =temp;
+			i++;
+			j--;
+		}
+		
+	}
+
+	console.log("Partion Sorted: " +list.slice(left, right+1));
+	console.log("List After Partion Sort list: " + list);
+	if (left < j)
+		quicksort(list, left, j);
+	if (i <right)
+		quicksort(list, i, right);
+
+	return list;
+}
